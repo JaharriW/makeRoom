cf5c88d291abd8fc04339d4795cf8b30a301bfc3
JaharriW <31023146+JaharriW@users.noreply.github.com>
2018-01-29 19:49:38 -0600

    Initial commit

---
+++ Hotel.java
@@ -0,0 +1,20 @@
+package makeRoom;
+import java.util.ArrayList;
+public class Hotel 
+{
+	String hotelName = "";
+	int totalRooms = 0;
+	private ArrayList<Room> rooms;
+	
+	
+	public Hotel()
+	{
+		String hotelName = "";
+		int totalRooms = 0;
+		ArrayList<Room>rooms = new ArrayList<Room>();
+		
+		
+	}
+	
+
+}
---
+++ Room.java
@@ -0,0 +1,16 @@
+package makeRoom;
+
+public class Room 
+{
+	int roomNumber = 0;
+	int daysRented = 0;
+	String roomType = "";
+	String occupantType = "";
+	
+	
+	public Room(int roomNumber, int daysRented, String roomType, String occupantType)
+	{
+		
+	}
+
+}
